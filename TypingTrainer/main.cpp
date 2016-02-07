6c0a906e843ca337eea0193d921c5723c7ab1467
kohki nakaji <boomerang.cube@gmail.com>
2016-02-08 04:42:24 +0900

    Initial Commit

---
+++ TypingTrainer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TypingTrainer
+//
+//  Created by 中地功貴 on 2016/02/08.
+//  Copyright © 2016年 中地功貴. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
